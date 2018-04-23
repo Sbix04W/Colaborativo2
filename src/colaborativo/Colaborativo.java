72fd4c31abda74e1d80d6dc3d9e067b5a0d57dda
CHRISS <CHRISS@IAN>
2018-04-23 09:53:54 -0500

    saludos

--- src/colaborativo/Colaborativo.java
+++ src/colaborativo/Colaborativo.java
@@ -20,7 +20,7 @@ public class Colaborativo {
         System.out.println("Luis Fernando");
         System.out.println("Omar Fonseca");
         System.out.println("Christian Chasi les saluda compañeros");
-        
+        System.out.println("Hola");
         System.out.println("dasdsa");
         System.out.println("aaa");
         System.out.println("V3");
